30cfcfeabb301c8a833b4825e3575095a31f6ea3
Kevin.Juanillo <Kevin.Juanillo@vertiv.com>
2024-05-23 23:00:11 +0800

    [KJ]: comments and clean up

--- storyGenerator.js
+++ storyGenerator.js
@@ -1,11 +1,11 @@
 function generateStory() {
+    //declarations
     const form = document.getElementById('story-form');
-    const noun = form.elements['noun'].value || "&lt;missing text&gt;";
+    const noun = form.elements['noun'].value || "&lt;missing text&gt;"; //instead of <missing text>
     const fistVerbPast = form.elements['verb-past'].value || "&lt;missing text&gt;";
     const secondVerbPast = form.elements['verb-present'].value || "&lt;missing text&gt;";
     const firstAdjective = form.elements['adjective'][0].value || "&lt;missing text&gt;";
-    const secondAdjective = form.elements['adjective'][1].value || "&lt;missing text&gt;";
-   
+    const secondAdjective = form.elements['adjective'][1].value || "&lt;missing text&gt;";  
     const storyTemplate = '<div id="story-paragraph" style="color:blue; font-family: comic sans-serif;">It was a dark stormy night. A shot rang out! A ' + noun + ' slammed. The maid ' + fistVerbPast + '. Suddenly, a ' + firstAdjective + ' ship appeared on the horizon! While millions of people ' + secondVerbPast + '. The king lived in luxury. Meanwhile, on a ' + secondAdjective + ' farm in Kansas, a boy was growing up.</div> <div style="color:blue;"><p> The End</p></div>'
 
     document.getElementById('input-section').style.display = "none";
@@ -15,7 +15,7 @@ function generateStory() {
 
 
     var str = document.getElementById("story-paragraph").innerHTML; 
-    var res = str.replace(new RegExp("&lt;missing text&gt;", 'g'), "<span style='color:red'>&lt;missing text&gt;</span>");
+    var res = str.replace(new RegExp("&lt;missing text&gt;", 'g'), "<span style='color:red'>&lt;missing text&gt;</span>"); //use replace to change color of rendered "<missing text>"
     document.getElementById("story-paragraph").innerHTML = res;
 
     
@@ -30,6 +30,5 @@ function returnToForm() {
 }
 
 function clearForm() {
-    const form = document.getElementById('story-form');
-    form.reset();
+    document.getElementById('story-form').reset();
 }
